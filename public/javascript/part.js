d91858e04e3c0c4fb4c5a2a0905c3686dee7b0ad
Tevin Temu <32607464+Temutev@users.noreply.github.com>
2020-02-13 12:08:08 +0300

    Revert "Initial commit"
    This reverts commit 85b3338fcc7f84467a86508320bd99c25a66d77a.

--- public/javascript/part.js
+++
@@ -1,9 +0,0 @@
-const thingspeak = document.querySelector("p");
-
-var resultArr = [];
-resultArr.push(thingspeak.dataset.visual);
-console.log(resultArr);
-
-resultArray.push(thingspeak.dataset.visual);
-thingspeak.dataset.visual;
-console.log(resultArray[0]);
